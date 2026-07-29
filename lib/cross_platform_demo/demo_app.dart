dfdf5ff0b887b81184a646b7486184778497e5a3
HongHac <144915081+chuonghoai@users.noreply.github.com>
2026-07-29 17:29:52 +0700

    feat: build architecture

--- lib/cross_platform_demo/demo_app.dart
+++ lib/cross_platform_demo/demo_app.dart
@@ -2,23 +2,20 @@ import 'package:flutter/material.dart';
 import 'data/location_repository.dart';
 import 'ui/responsive/responsive_layout.dart';
 
-/// The entry point for the Cross-Platform Demo.
 class DemoApp extends StatelessWidget {
   const DemoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Instantiate the shared repository ONCE at the top level of the demo
     final sharedRepository = LocationRepository();
 
     return MaterialApp(
-      title: 'SmartSpace Cross-Platform Demo',
+      title: 'Smart Space',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      // The ResponsiveLayout will decide whether to show Mobile or Web UI
       home: ResponsiveLayout(repository: sharedRepository),
     );
   }
