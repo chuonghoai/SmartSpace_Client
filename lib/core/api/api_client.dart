9ff097297f0c08c6e6cbac10ff1a3ba5180b8b90
HongHac <144915081+chuonghoai@users.noreply.github.com>
2026-08-12 22:18:34 +0700

    feat: implementation repo for auth features

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -1,49 +1,89 @@
 import 'package:dio/dio.dart';
+import 'package:smartspace_client/core/api/api_response.dart';
 import 'dio_client.dart';
 
 class ApiClient {
-  Future<T> get<T>(String url, {Options? options, Map<String, dynamic>? queryParameters}) async {
-    final response = await dioInstance.get<T>(
+  Future<ApiResponse<T>> get<T>(
+    String url, {
+    Options? options,
+    Map<String, dynamic>? queryParameters,
+    T Function(dynamic)? decoder,
+  }) async {
+    final response = await dioInstance.get<Map<String, dynamic>>(
       url,
       options: options,
       queryParameters: queryParameters,
     );
-    return response.data as T;
+    return ApiResponse.fromJson(
+      response.data!,
+      decoder ?? (json) => json as T,
+    );
   }
 
-  Future<T> post<T>(String url, {dynamic data, Options? options}) async {
-    final response = await dioInstance.post<T>(
+  Future<ApiResponse<T>> post<T>(
+    String url, {
+    dynamic data,
+    Options? options,
+    T Function(dynamic)? decoder,
+  }) async {
+    final response = await dioInstance.post<Map<String, dynamic>>(
       url,
       data: data,
       options: options,
     );
-    return response.data as T;
+    return ApiResponse.fromJson(
+      response.data!,
+      decoder ?? (json) => json as T,
+    );
   }
 
-  Future<T> put<T>(String url, {dynamic data, Options? options}) async {
-    final response = await dioInstance.put<T>(
+  Future<ApiResponse<T>> put<T>(
+    String url, {
+    dynamic data,
+    Options? options,
+    T Function(dynamic)? decoder,
+  }) async {
+    final response = await dioInstance.put<Map<String, dynamic>>(
       url,
       data: data,
       options: options,
     );
-    return response.data as T;
+    return ApiResponse.fromJson(
+      response.data!,
+      decoder ?? (json) => json as T,
+    );
   }
 
-  Future<T> patch<T>(String url, {dynamic data, Options? options}) async {
-    final response = await dioInstance.patch<T>(
+  Future<ApiResponse<T>> patch<T>(
+    String url, {
+    dynamic data,
+    Options? options,
+    T Function(dynamic)? decoder,
+  }) async {
+    final response = await dioInstance.patch<Map<String, dynamic>>(
       url,
       data: data,
       options: options,
     );
-    return response.data as T;
+    return ApiResponse.fromJson(
+      response.data!,
+      decoder ?? (json) => json as T,
+    );
   }
 
-  Future<T> delete<T>(String url, {Options? options}) async {
-    final response = await dioInstance.delete<T>(
+  Future<ApiResponse<T>> delete<T>(
+    String url, {
+    Options? options,
+    T Function(dynamic)? decoder,
+  }) async {
+    final response = await dioInstance.delete<Map<String, dynamic>>(
       url,
       options: options,
     );
-    return response.data as T;
+    return ApiResponse.fromJson(
+      response.data!,
+      decoder ?? (json) => json as T,
+    );
   }
 }
 
